e1f04cb2486e6e4c4c2f324f3e4f1b1cdaf191f7
root <root@localhost.localdomain>
2018-09-13 09:50:05 -0400

    dl silly.sh

--- profile.py
+++ profile.py
@@ -15,6 +15,7 @@ node = request.XenVM("node")
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
 node.routable_control_ip = "true"
 # Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="/bin/sh", command="/local/silly.sh"))
+node.addService(pg.Install(url=https://"github.com/mrblint/my-profile/silly.sh",path="~"
+node.addService(pg.Execute(shell="/bin/sh", command="~/silly.sh"))
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
