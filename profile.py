7785508a323157b6231f896a832fde304dd3d43b
root <root@localhost.localdomain>
2018-09-13 02:23:45 -0400

    fixed imageURN

--- profile.py
+++ profile.py
@@ -1,10 +1,3 @@
-"""This is a trivial example of a gitrepo-based profile; The profile source code and other software, documentation, etc. are stored in in a publicly accessible GIT repository (say, github.com). When you instantiate this profile, the repository is cloned to all of the nodes in your experiment, to `/local/repository`. 
-
-This particular profile is a simple example of using a single raw PC. It can be instantiated on any cluster; the node will boot the default operating system, which is typically a recent version of Ubuntu.
-
-Instructions:
-Wait for the profile instance to start, then click on the node in the topology and choose the `shell` menu item. 
-"""
 
 # Import the Portal object.
 import geni.portal as portal
@@ -19,7 +12,7 @@ request = pc.makeRequestRSpec()
  
 # Add a raw PC to the request.
 node = request.XenVM("node")
-node.disk_image= "urn:publicated:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+node.disk_image= "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
 node.routeable_control_ip="true"
 # Install and execute a script that is contained in the repository.
 node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
