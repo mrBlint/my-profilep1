fb270abd677dd993952fd7e45c1ededca78e2cd5
root <root@localhost.localdomain>
2018-09-13 10:10:03 -0400

    git time

--- profile.py
+++ profile.py
@@ -15,7 +15,8 @@ node = request.XenVM("node")
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
 node.routable_control_ip = "true"
 # Install and execute a script that is contained in the repository.
-node.addService(pg.Install(url=https://"github.com/mrblint/my-profile/silly.sh",path="~"))
-node.addService(pg.Execute(shell="/bin/sh", command="~/silly.sh"))
+node.addService(pg.Execute(shell="/bin/sh", command="sudo yum -y install git"))
+node.addService(pg.Execute(shell="/bin/sh", command="git clone https://github.com/mrblint/my-profile"))
+node.addService(pg.Execute(shell="/bin/sh", command="my-profile/silly.sh"))
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
