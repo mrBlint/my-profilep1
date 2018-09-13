441912edce7107b833f9c68ae8e60a439194eb0c
root <root@localhost.localdomain>
2018-09-13 03:31:50 -0400

    fixed ip issue

--- profile.py
+++ profile.py
@@ -12,8 +12,8 @@ request = pc.makeRequestRSpec()
  
 # Add a raw PC to the request.
 node = request.XenVM("node")
-node.disk_image= "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-node.routeable_control_ip="true"
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+node.routable_control_ip = "true"
 # Install and execute a script that is contained in the repository.
 node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
 node.addService(pg.Execute(shell="sh",command="systemctl restart httpd.service"))
